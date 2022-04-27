8e6d58d3b0af73840b9e249caa9eb770d6a8f971
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-04-27 03:37:02 -0700

    signup form

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path
 
 from myapp import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('accounts/signup', views.signup, name='signup')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from myapp.form import SignupForm
+
+
 def home(request):
     return render(request, 'myapp/index.html')
 
+
 def signup(request):
-    return render(request,'registration/signup.html')
+    form = SignupForm
+    return render(request, 'registration/signup.html', {'form': form})
