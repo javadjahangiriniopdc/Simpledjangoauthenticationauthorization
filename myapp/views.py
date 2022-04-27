8e73afb0ea939dbfb232f65c82cb8089930b940e
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-04-27 03:02:15 -0700

    add login and logout

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'myapp/index.html')
+
+def signup(request):
+    return render(request,'registration/signup.html')
