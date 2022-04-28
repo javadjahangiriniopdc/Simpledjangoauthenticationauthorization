9cde4ceeee35d2e180a9c577bd164880cc1b0fc1
drjj007 <javad.jahangiri.niopdc@gmail.com>
2022-04-27 23:10:34 -0700

    add registration

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 from myapp.form import SignupForm
 
@@ -9,5 +9,15 @@ def home(request):
 
 
 def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            pwd = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=pwd)
+            login(request, user)
+            return redirect('home')
+
     form = SignupForm
     return render(request, 'registration/signup.html', {'form': form})
